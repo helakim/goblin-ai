fc8318090ca1e376ff5ea1e55b717a9b6966499a
hela.kim <firefoxdev0619@gmail.com>
2019-09-30 11:43:40 +0900

    face_detection[Only-U] (add model summary) by kt.kim

---
+++ pytorch_version/face_detection/cfg/model_summary.py
@@ -0,0 +1,132 @@
+# *********************************************************************
+# @Project    goblin-ai
+# @FILE       kyung_tae_kim (hela.kim)
+# @Copyright: hela
+# @Created:   30/09/2019
+#
+#            7''  Q..\
+#         _7         (_
+#       _7  _/    _q.  /
+#     _7 . ___  /VVvv-'_                                            .
+#    7/ / /~- \_\\      '-._     .-'                      /       //
+#   ./ ( /-~-/||'=.__  '::. '-~'' {             ___   /  //     ./{
+#  V   V-~-~| ||   __''_   ':::.   ''~-~.___.-'' _/  // / {_   /  {  /
+#   VV/-~-~-|/ \ .'__'. '.    '::                     _ _ _        ''.
+#   / /~~~~||VVV/ /  \ )  \        _ __ ___   ___ ___(_) | | __ _   .::'
+#  / (~-~-~\\.-' /    \'   \::::. | '_ ` _ \ / _ \_  / | | |/ _` | :::'
+# /..\    /..\__/      '     '::: | | | | | | (_) / /| | | | (_| | ::
+# vVVv    vVVv                 ': |_| |_| |_|\___/___|_|_|_|\__,_| ''
+#
+# *********************************************************************
+import torch
+import torch.nn as nn
+import numpy as np
+from collections import OrderedDict
+from logger import logger as cerberus
+
+
+def summary(model, input_size, batch_size=-1, device="cuda"):
+    def register_hook(module):
+
+        def hook(module, input, output):
+            class_name = str(module.__class__).split(".")[-1].split("'")[0]
+            module_idx = len(summary)
+
+            m_key = "%s-%i" % (class_name, module_idx + 1)
+            summary[m_key] = OrderedDict()
+            summary[m_key]["input_shape"] = list(input[0].size())
+            summary[m_key]["input_shape"][0] = batch_size
+            if isinstance(output, (list, tuple)):
+                summary[m_key]["output_shape"] = [
+                    [-1] + list(o.size())[1:] for o in output
+                ]
+            else:
+                summary[m_key]["output_shape"] = list(output.size())
+                summary[m_key]["output_shape"][0] = batch_size
+
+            params = 0
+            if hasattr(module, "weight") and hasattr(module.weight, "size"):
+                params += torch.prod(torch.LongTensor(list(module.weight.size())))
+                summary[m_key]["trainable"] = module.weight.requires_grad
+            if hasattr(module, "bias") and hasattr(module.bias, "size"):
+                params += torch.prod(torch.LongTensor(list(module.bias.size())))
+            summary[m_key]["nb_params"] = params
+
+        if (
+                not isinstance(module, nn.Sequential)
+                and not isinstance(module, nn.ModuleList)
+                and not (module == model)
+        ):
+            hooks.append(module.register_forward_hook(hook))
+
+    device = device.lower()
+    assert device in [
+        "cuda",
+        "cpu",
+    ], "Input device is not valid, please specify 'cuda' or 'cpu'"
+
+    if device == "cuda" and torch.cuda.is_available():
+        dtype = torch.cuda.FloatTensor
+    else:
+        dtype = torch.FloatTensor
+
+    # multiple inputs to the network
+    if isinstance(input_size, tuple):
+        input_size = [input_size]
+
+    # batch_size of 2 for batchnorm
+    x = [torch.rand(2, *in_size).type(dtype) for in_size in input_size]
+    # print(type(x[0]))
+
+    # create properties
+    summary = OrderedDict()
+    hooks = []
+
+    # register hook
+    model.apply(register_hook)
+
+    # make a forward pass
+    # print(x.shape)
+    model(*x)
+
+    # remove these hooks
+    for h in hooks:
+        h.remove()
+
+    print("----------------------------------------------------------------")
+    line_new = "{:>20}  {:>25} {:>15}".format("Layer (type)", "Output Shape", "Param #")
+    print(line_new)
+    print("================================================================")
+    total_params = 0
+    total_output = 0
+    trainable_params = 0
+    for layer in summary:
+        # input_shape, output_shape, trainable, nb_params
+        line_new = "{:>20}  {:>25} {:>15}".format(
+            layer,
+            str(summary[layer]["output_shape"]),
+            "{0:,}".format(summary[layer]["nb_params"]),
+        )
+        total_params += summary[layer]["nb_params"]
+        total_output += np.prod(summary[layer]["output_shape"])
+        if "trainable" in summary[layer]:
+            if summary[layer]["trainable"] == True:
+                trainable_params += summary[layer]["nb_params"]
+        print(line_new)
+
+    # assume 4 bytes/number (float on cuda).
+    total_input_size = abs(np.prod(input_size) * batch_size * 4. / (1024 ** 2.))
+    total_output_size = abs(2. * total_output * 4. / (1024 ** 2.))  # x2 for gradients
+    total_params_size = abs(total_params.numpy() * 4. / (1024 ** 2.))
+    total_size = total_params_size + total_output_size + total_input_size
+    # TODO.md: Add terminal table [level: 1]
+    cerberus.info('================================================================')
+    cerberus.info("total parameters: {0:,}".format(total_params))
+    cerberus.info("trainable parameters: {0:,}".format(trainable_params))
+    cerberus.error("non-trainable parameters: {0:,}".format(total_params - trainable_params))
+    cerberus.info('================================================================')
+    cerberus.info("input size (MB): {:.2f}".format(total_input_size))
+    cerberus.info("forward / backward pass size (MB) {:.2f}".format(total_output))
+    cerberus.info("parameters size (MB): {:.2f}".format(total_output_size))
+    cerberus.info("estimated total size (MB) {:.2f}".format(total_size))
+    cerberus.info('================================================================')
