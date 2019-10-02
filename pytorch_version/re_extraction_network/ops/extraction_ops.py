8257a550bc03b891684b858cb4459424b8f55779
hela.kim <firefoxdev0619@gmail.com>
2019-10-02 11:56:20 +0900

    Re-Extraction-Network[Argos-EYE] (add feature extraction operations) by kt.kim

--- pytorch_version/re_extraction_network/ops/extraction_ops.py
+++ pytorch_version/re_extraction_network/ops/extraction_ops.py
@@ -18,3 +18,37 @@
 # vVVv    vVVv                 ': |_| |_| |_|\___/___|_|_|_|\__,_| ''
 #
 # *********************************************************************
+import torch
+import torch.nn as nn
+
+
+class FeatureExtraction(nn.Module):
+
+    def __init__(self, sub_module, extracted_layers):
+        """
+        Args:
+            :param sub_module: named sub module (e.g. max pooling, convolution, dropout layer)
+            :param extracted_layers: extraction weights by sub module
+        """
+        super(FeatureExtraction, self).__init__()
+        self.sub_module = sub_module
+        self.extracted_layers = extracted_layers
+
+    def forward(self, x: torch.Tensor):
+        outputs_feature = []
+        # ------
+        # named modules
+        for name, module in self.submodule._modules.items():
+            if name == 'classifier':
+                # fully connected layers
+                x = x.view(x.size(0), -1)
+
+            if name == 'base':
+                for block_name, cnn_block in module._modules.items():
+                    x = cnn_block(x)
+                    # -------
+                    # extraction feature maps (fc7)
+                    if block_name == self.extracted_layers:
+                        outputs_feature.append(x)
+
+        return outputs_feature
