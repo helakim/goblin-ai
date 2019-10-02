93317690a512f6a38e650cb07c4bcb8ae4213aaa
hela.kim <firefoxdev0619@gmail.com>
2019-10-02 11:49:01 +0900

    Re-Extraction-Network[Argos-EYE] (add channel-wise pooling) by kt.kim

--- pytorch_version/re_extraction_network/ops/single_layer.py
+++ pytorch_version/re_extraction_network/ops/single_layer.py
@@ -18,3 +18,44 @@
 # vVVv    vVVv                 ': |_| |_| |_|\___/___|_|_|_|\__,_| ''
 #
 # *********************************************************************
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def pool_2d(tensor):
+    """
+    Args:
+        :param tensor: input 2-dimensional tensor (extraction convolution weights)
+        :param pool_type: pooling type [chooses max pooling, average pooling]
+    """
+    input_spec = tensor.size()
+    dilated_kernel = (np.int(input_spec[2] / 8), np.int(input_spec[3]))
+    x = F.max_pool2d(tensor, kernel_size=dilated_kernel)
+
+    x = x[0].cpu().data.numpy()
+    x = np.transpose(x, (2, 1, 0))[0]
+
+    return x
+
+
+class ChannelWiseMaxPool(nn.Module):
+    def __init__(self, pool_type='dilated_kernel'):
+        super(ChannelWiseMaxPool, self).__init__()
+        self.pool_type = pool_type
+
+    def forward(self, input_tensor):
+        """
+        Args:
+            :param input_tensor: input 2-dimensional tensor (extraction convolution weights)
+        """
+        x_spec = input_tensor.size()
+
+        if self.dilated_kernel == 'dilated_kernel':
+            kernel_size = (1, x_spec[3])
+            pooled = F.max_pool2d(input=x_spec, kernel_size=kernel_size)
+        elif self.dilated_kernel == 'atrous_kernel':
+            kernel_size = (3, x_spec[3])
+            pooled = F.avg_pool2d(input=x_spec, kernel_size=kernel_size)
+
+        return pooled
