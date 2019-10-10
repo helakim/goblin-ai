55300ce0db43c4dccc5d0be134eea5de78c38e2a
hela.kim <firefoxdev0619@gmail.com>
2019-10-10 17:10:32 +0900

    Goblin-AI Application [Quasar] (add const.js for sildebar) by kt.kim

---
+++ application/quasar/src/const/const.js
@@ -0,0 +1,12 @@
+const CONST = {
+    menuItems: [
+        { name: '메인', icon: 'desktop_mac', routerLink: '/Main' },
+        { name: '키워드 트렌드', icon: 'whatshot', routerLink: '/KeywordTrend' },
+        { name: '통계', icon: 'show_chart', routerLink: '/Statist' },
+        { name: '관심 키워드 분석', icon: 'favorite', routerLink: '/Favorite' },
+        { name: '검색', icon: 'search', routerLink: '/Search' },
+        { name: '트렌드 비교', icon: 'trending_up', routerLink: '/TrendCompare' },
+    ],
+};
+
+export default CONST;
